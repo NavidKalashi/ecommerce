1e5b92b9bb3acca7965acf2aafe3608952157a88
Navid Kalashi <kalashinavid@gmail.com>
2023-07-07 15:17:21 +0330

    updateItem view

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -11,7 +11,28 @@ for(var i = 0; i < updateBtns.length; i++){
             console.log('user is not authenticated!')
         }
         else{
-            console.log('user is authenticated, sending data...')
+            updateUserOrder(productId, action)
         }
+    })
+}
+
+function updateUserOrder(productId, action){
+    console.log('user is authenticated, sending data...')
+
+    var url = '/update_item/'
+
+    fetch(url, {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken
+        },
+        body: JSON.stringify({'productId': productId, 'action': action})
+    })
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) =>{
+        console.log('data:', data)
     })
 }
